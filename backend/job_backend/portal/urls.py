36fb427a024437bbfcdb070adb7d64e3cc65675e
Ameya Phatak <ameyaphatak88@gmail.com>
2020-07-12 11:59:12 +0530

    Thanks page done

--- backend/job_backend/portal/urls.py
+++ backend/job_backend/portal/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name = "index"),
     path('postjob/', views.simpleform, name = "simpleform"),
+    path('postjob/thanks/', views.thanks, name = "thanks"),
 ]
--- backend/job_backend/portal/views.py
+++ backend/job_backend/portal/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .forms import NameForm
+from django.http import HttpResponseRedirect
 
 def index(request):
     template = loader.get_template('portal/index.html')
@@ -11,8 +13,15 @@ def index(request):
 
 
 def simpleform(request):
-    template = loader.get_template('portal/simpleform.html')
-    context = {
-        
-    }
-    return HttpResponse(template.render(context,request))
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = NameForm()
+    return render(request, 'portal/simpleform.html', {'form': form})
+
+
+def thanks(request):
+    template = loader.get_template('portal/thanks.html')
+    return HttpResponse(template.render({}, request))
